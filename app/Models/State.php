c52595b4885aab6471d17fbcfec23ab69ee5042b
Jawarians <80420780+Jawarians@users.noreply.github.com>
2025-10-02 15:11:33 +0800

    SQ-1032-add-function-new-quarry-from-merchant (#20)

--- app/Models/State.php
+++ app/Models/State.php
@@ -6,4 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class State extends Model
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'address_states';
+    
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'name';
+    
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+    
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
 }
